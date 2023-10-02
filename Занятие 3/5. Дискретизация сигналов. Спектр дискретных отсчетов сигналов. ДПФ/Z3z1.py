b3ca4607e644ad6dfa9c7cc999d21e780a2276b5
SeregaSH2108 <Sestakovs700@gmail.com>
2023-10-02 16:10:48 +0700

    Добавлено занятие 3

--- Занятие 3/5. Дискретизация сигналов. Спектр дискретных отсчетов сигналов. ДПФ/Z3z1.py
+++ Занятие 3/5. Дискретизация сигналов. Спектр дискретных отсчетов сигналов. ДПФ/Z3z1.py
@@ -1,22 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.signal import kaiserord, lfilter, firwin, freqz
 from scipy import fftpack
-
-Ts1 = 2e-3 # Интервал дискретизации
+ 
+Ts1=1e-3 # Интервал дискретизации
 fs1 = 1/Ts1
-f0 = (0.4*np.pi*fs1)/(2*np.pi)#определение значения аналоговой частоты
+f = (0.4*np.pi*fs1)/(2*np.pi)#определение значения аналоговой частоты
 t = np.arange(-5, 11)*Ts1
-   
+s =  np.cos(f*t*(2*np.pi))
+plt.figure(1)
+plt.subplot(3,1,1)
+plt.plot(t, s)
+plt.subplot(3,1,2)
+plt.stem(t,s)
+print("Аналоговая частота", f)
 
-    
+plt.figure(2)
 
-for f in range(25, 226, 50):
-    s = np.cos(f*t*(2*np.pi))
-    sp = fftpack.fft(s) 
-    plt.figure()
-    freqs=np.arange(0,fs1,fs1/len(s))
-    plt.stem(freqs, np.abs(sp))
-    plt.xlabel('Частота в герцах')
-    plt.ylabel('Модуль спектра')
-    plt.title('Спектр отсчетов')
-    plt.grid()
+
+sp = fftpack.fft(s)
+freqs=np.arange(0,fs1,fs1/len(s))
+plt.stem(freqs, np.abs(sp))
+plt.xlabel('Частота в герцах [Hz]')
+plt.ylabel('Модуль спектра')
